2e2a5d184a2052ddaa3865bfcb5ce3ba19d98aa4
heowc <heowc1992@gmail.com>
2019-11-09 17:13:19 +0900

    Change deprecated people api to user info api

--- src/main/java/com/example/demo/SimpleGoogleOauth2Controller.java
+++ src/main/java/com/example/demo/SimpleGoogleOauth2Controller.java
@@ -1,17 +1,6 @@
 package com.example.demo;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import javax.servlet.http.HttpSession;
-
-import org.apache.commons.codec.binary.StringUtils;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +12,11 @@ import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class SimpleGoogleOauth2Controller {
@@ -37,16 +31,13 @@ public class SimpleGoogleOauth2Controller {
 	}
 
 	@RequestMapping("/oauth2/google")
-	public View auth(HttpSession session) {
-
-		session.setAttribute("state", UUID.randomUUID().toString());
-
+	public View auth() {
 		UriComponents googleOauthUrl = UriComponentsBuilder.fromHttpUrl(properties.getAuth().getUrl())
 				.queryParam("client_id", properties.getClientId())
 				.queryParam("response_type", "code")
 				.queryParam("scope", "openid email profile")
 				.queryParam("redirect_uri", properties.getAuth().getCallbackUrl())
-				.queryParam("state", session.getAttribute("state"))
+				.queryParam("state", UUID.randomUUID().toString())
 				.queryParam("access_type", "offline")
 				.queryParam("approval_prompt", "force")
 				.build();
@@ -57,12 +48,6 @@ public class SimpleGoogleOauth2Controller {
 	@ResponseBody
 	@RequestMapping("/oauth2/google/callback")
 	public ResponseEntity<String> authCallback(HttpSession session, @RequestParam String state, @RequestParam String code) {
-
-		String sessionState = (String) session.getAttribute("state");
-
-		if (!StringUtils.equals(sessionState, state)) {
-			return ResponseEntity.badRequest().build();
-		}
 
 		UriComponents googleTokenUrl = UriComponentsBuilder.fromHttpUrl(properties.getToken().getUrl()).build();
 
@@ -91,11 +76,11 @@ public class SimpleGoogleOauth2Controller {
 		}
 
 		String accessToken = authorization.split(" ")[1];
-		UriComponents googleUserUrl = UriComponentsBuilder.fromHttpUrl("https://www.googleapis.com/plus/v1/people/me").build();
+		UriComponents googleUserUrl = UriComponentsBuilder.fromHttpUrl("https://www.googleapis.com/oauth2/v2/userinfo").build();
 
 		try {
 			HttpHeaders headers = new HttpHeaders();
-			headers.setBearerAuth(accessToken);
+			headers.set("Authorization", "Bearer " + accessToken);
 			return restTemplate.exchange(googleUserUrl.toUriString(), HttpMethod.GET, new HttpEntity<>(headers), String.class);
 		} catch (RestClientException e) {
 			e.printStackTrace();
--- src/main/java/com/example/demo/SpringSocialGoogleOauth2Controller.java
+++ src/main/java/com/example/demo/SpringSocialGoogleOauth2Controller.java
@@ -1,13 +1,8 @@
 package com.example.demo;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.social.google.api.impl.GoogleTemplate;
-import org.springframework.social.google.api.plus.Person;
+import org.springframework.social.google.api.oauth2.UserInfo;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
 import org.springframework.social.oauth2.AccessGrant;
 import org.springframework.social.oauth2.GrantType;
@@ -22,52 +17,56 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/social")
 public class SpringSocialGoogleOauth2Controller {
 
-	private GoogleConnectionFactory connectionFactory;
+    private GoogleConnectionFactory connectionFactory;
 
-	private OAuth2Parameters parameters;
+    private OAuth2Parameters parameters;
 
-	public SpringSocialGoogleOauth2Controller(GoogleConnectionFactory connectionFactory, OAuth2Parameters parameters) {
-		this.connectionFactory = connectionFactory;
-		this.parameters = parameters;
-	}
+    public SpringSocialGoogleOauth2Controller(GoogleConnectionFactory connectionFactory, OAuth2Parameters parameters) {
+        this.connectionFactory = connectionFactory;
+        this.parameters = parameters;
+    }
 
-	@RequestMapping("/oauth2/google")
-	public View auth() {
-		return new RedirectView(connectionFactory.getOAuthOperations().buildAuthenticateUrl(GrantType.AUTHORIZATION_CODE, parameters));
-	}
+    @RequestMapping("/oauth2/google")
+    public View auth() {
+        return new RedirectView(connectionFactory.getOAuthOperations().buildAuthenticateUrl(GrantType.AUTHORIZATION_CODE, parameters));
+    }
 
-	@ResponseBody
-	@RequestMapping("/oauth2/google/callback")
-	public ResponseEntity<String> authCallback(HttpSession session, @RequestParam String code) {
-		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-		map.set("grant_type", "authorization_code");
-		AccessGrant accessGrant = connectionFactory.getOAuthOperations().exchangeForAccess(code, parameters.getRedirectUri(), map);
+    @ResponseBody
+    @RequestMapping("/oauth2/google/callback")
+    public ResponseEntity<String> authCallback(HttpSession session, @RequestParam String code) {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.set("grant_type", "authorization_code");
+        AccessGrant accessGrant = connectionFactory.getOAuthOperations().exchangeForAccess(code, parameters.getRedirectUri(), map);
 
-		Map<String, String> body = new HashMap<>();
-		body.put("access_token", accessGrant.getAccessToken());
-		body.put("refresh_token", accessGrant.getRefreshToken());
-		body.put("scope", accessGrant.getScope());
-		body.put("expire_time", accessGrant.getExpireTime().toString());
-		return ResponseEntity.ok(body.toString());
-	}
+        Map<String, String> body = new HashMap<>();
+        body.put("access_token", accessGrant.getAccessToken());
+        body.put("refresh_token", accessGrant.getRefreshToken());
+        body.put("scope", accessGrant.getScope());
+        body.put("expire_time", accessGrant.getExpireTime().toString());
+        return ResponseEntity.ok(body.toString());
+    }
 
-	@ResponseBody
-	@RequestMapping("/me")
-	public ResponseEntity<String> getGoogleUser(@RequestHeader("Authorization") String authorization) {
+    @ResponseBody
+    @RequestMapping("/me")
+    public ResponseEntity<UserInfo> getGoogleUser(@RequestHeader("Authorization") String authorization) {
 
-		if (!authorization.startsWith("Bearer")) {
-			return ResponseEntity.badRequest().build();
-		}
+        if (!authorization.startsWith("Bearer")) {
+            return ResponseEntity.badRequest().build();
+        }
 
-		String accessToken = authorization.split(" ")[1];
-		GoogleTemplate google = new GoogleTemplate(accessToken);
-		Person me = google.plusOperations().getGoogleProfile();
+        String accessToken = authorization.split(" ")[1];
+        GoogleTemplate google = new GoogleTemplate(accessToken);
+        UserInfo user = google.oauth2Operations().getUserinfo();
 
-		return ResponseEntity.ok(me.getAccountEmail());
-	}
+        return ResponseEntity.ok(user);
+    }
 
 }
